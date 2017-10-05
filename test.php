12102620380f56a4c2a6331a09a1da869c366a2c
Uğur Biçer <uuur86@yandex.com>
2017-10-05 16:15:50 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,30 +1,28 @@
 <?php
 
-include_once 'ubdb.php';
+include_once 'class.php';
 include_once 'inc.php';
+/*
+    for($t=1;$t<1000;$t++){
+    $gId   = rand(1, 2000);
+    $gNo   = rand(1, 2000);
+    $gPass = rand(51234,99999935);
+    
+    $ubdb->define('username','ugur'.$gNo);
+    $ubdb->define('password',$gPass);
 
-    for($t=1;$t<100;$t++){
-    $gArmor  = rand(5, 30);
-    $gHealth = rand(900,2000);
-    $gResist = rand(5,35);
-    $gPower  = rand(36,40);
-    $gMagic  = rand(30,90);
-    $gOwner  = rand(1,1000);
-    
-    $ubdb->define('username','uğ'.$gArmor);
-    $ubdb->define('password',$gHealth);
-    
     $ubdb->add('users');
     
-    $ubdb->define('userid','uğ'.$gResist);
+    $ubdb->define('userid','ugur'.$gId);
     $ubdb->add('cart');
     }
-    
+    */
 $ubdb->save();
     
-$get1 = $ubdb->get("users","username","uğ%");
-$get2 = $ubdb->get("cart","userid","uğ%");
+$get1 = $ubdb->get("users", "username", array("ugur",'%','1'));
+$get2 = $ubdb->get("cart", "userid", array("ug",'%','1'));
 
-$results = $ubdb->join($get1,$get2,"username","userid");
+$results = $ubdb->join($get1, $get2, "username", "userid");
+
 
 print_r($results);
